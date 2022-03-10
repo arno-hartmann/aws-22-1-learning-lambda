cf21e31fb3af7d5061bafd813d32bfb47714f3bd
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-10 12:14:50 +0100

    Commented and updated

--- first-lambda.py
+++ first-lambda.py
@@ -1,13 +1,13 @@
 import json
 import boto3
 
-client = boto3.client('sns')
+snsclient = boto3.client('sns')
 s3client = boto3.client('s3')
 
 def lambda_handler(event, context):
   
-   key = event['Records'][0]['s3']['object']['key']
    bucket = event['Records'][0]['s3']['bucket']['name']
+   key = event['Records'][0]['s3']['object']['key']
 
    response = s3client.get_object(
       Bucket = bucket,
@@ -22,4 +22,6 @@ def lambda_handler(event, context):
    message = 'Number of words in text file {} is {}'.format(key, numberOfWords)
    
    print(message)
-   client.publish(TargetArn = "arn:aws:sns:us-west-2:694046024538:drop-file-mail", Message = message)
+
+   # Use arn of the sns. Check for same Region!
+   snsclient.publish(TargetArn = "arn:aws:sns:us-west-2:694046024538:drop-file-mail", Message = message)
